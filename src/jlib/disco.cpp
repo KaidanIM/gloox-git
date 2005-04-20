123d9adda3737bbc9343a7920a5c4cc7708a6f9c
Jakob Schröter <js@camaya.net>
2005-04-20 14:01:22 +0000

    + add switch for iq type
    git-svn-id: svn://svn.camaya.net/gloox/trunk@303 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/jlib/disco.cpp
+++ src/jlib/disco.cpp
@@ -38,53 +38,70 @@ Disco::~Disco()
 
 void Disco::handleIq( const char* xmlns, ikspak* pak )
 {
-  if( iks_strncmp( XMLNS_VERSION, xmlns, iks_strlen( XMLNS_VERSION ) ) == 0 )
+  switch( pak->subtype )
   {
-    iks* x = iks_new( "iq" );
-    iks_insert_attrib( x, "type", "result" );
-    iks_insert_attrib( x, "to", pak->from->full );
-    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
-    iks_insert_attrib( x, "id", pak->id );
-    iks* y = iks_insert( x, "query" );
-    iks_insert_attrib( y, "xmlns", XMLNS_VERSION );
-    iks* z = iks_insert( y, "name" );
-    iks_insert_cdata( z, m_versionName.c_str(), m_versionName.length() );
-    z = iks_insert( y, "version" );
-    iks_insert_cdata( z, m_versionVersion.c_str(), m_versionVersion.length() );
-    m_parent->send( x );
-  }
-  else if( iks_strncmp( XMLNS_DISCO_INFO, xmlns, iks_strlen( XMLNS_DISCO_INFO ) ) == 0 )
-  {
-    iks* x = iks_new( "iq" );
-    iks_insert_attrib( x, "type", "result" );
-    iks_insert_attrib( x, "id", pak->id );
-    iks_insert_attrib( x, "to", pak->from->full );
-    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
-    iks* y = iks_insert( x, "query" );
-    iks_insert_attrib( y, "xmlns", XMLNS_DISCO_INFO );
-    iks* i = iks_insert( y, "identity" );
-    iks_insert_attrib( i, "category", m_identityCategory.c_str() );
-    iks_insert_attrib( i, "type", m_identityType.c_str() );
-    iks_insert_attrib( i, "name", m_versionName.c_str() );
+    case IKS_TYPE_GET:
+      if( iks_strncmp( XMLNS_VERSION, xmlns, iks_strlen( XMLNS_VERSION ) ) == 0 )
+      {
+        iks* x = iks_new( "iq" );
+        iks_insert_attrib( x, "type", "result" );
+        iks_insert_attrib( x, "to", pak->from->full );
+        iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+        iks_insert_attrib( x, "id", pak->id );
+        iks* y = iks_insert( x, "query" );
+        iks_insert_attrib( y, "xmlns", XMLNS_VERSION );
+        iks* z = iks_insert( y, "name" );
+        iks_insert_cdata( z, m_versionName.c_str(), m_versionName.length() );
+        z = iks_insert( y, "version" );
+        iks_insert_cdata( z, m_versionVersion.c_str(), m_versionVersion.length() );
+        m_parent->send( x );
+      }
+      else if( iks_strncmp( XMLNS_DISCO_INFO, xmlns, iks_strlen( XMLNS_DISCO_INFO ) ) == 0 )
+      {
+        iks* x = iks_new( "iq" );
+        iks_insert_attrib( x, "type", "result" );
+        iks_insert_attrib( x, "id", pak->id );
+        iks_insert_attrib( x, "to", pak->from->full );
+        iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+        iks* y = iks_insert( x, "query" );
+        iks_insert_attrib( y, "xmlns", XMLNS_DISCO_INFO );
+        iks* i = iks_insert( y, "identity" );
+        iks_insert_attrib( i, "category", m_identityCategory.c_str() );
+        iks_insert_attrib( i, "type", m_identityType.c_str() );
+        iks_insert_attrib( i, "name", m_versionName.c_str() );
 
-    StringList::const_iterator it = m_features.begin();
-    for( it; it != m_features.end(); ++it )
-    {
-      iks* z = iks_insert( y, "feature" );
-      iks_insert_attrib( z, "var", (*it).c_str() );
-    }
-    m_parent->send( x );
-  }
-  else if( iks_strncmp( XMLNS_DISCO_ITEMS, xmlns, iks_strlen( XMLNS_DISCO_ITEMS ) ) == 0 )
-  {
-    iks* x = iks_new( "iq" );
-    iks_insert_attrib( x, "type", "result" );
-    iks_insert_attrib( x, "id", pak->id );
-    iks_insert_attrib( x, "to", pak->from->full );
-    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
-    iks* y = iks_insert( x, "query" );
-    iks_insert_attrib( y, "xmlns", XMLNS_DISCO_ITEMS );
-    m_parent->send( x );
+        StringList::const_iterator it = m_features.begin();
+        for( it; it != m_features.end(); ++it )
+        {
+          iks* z = iks_insert( y, "feature" );
+          iks_insert_attrib( z, "var", (*it).c_str() );
+        }
+        m_parent->send( x );
+      }
+      else if( iks_strncmp( XMLNS_DISCO_ITEMS, xmlns, iks_strlen( XMLNS_DISCO_ITEMS ) ) == 0 )
+      {
+        iks* x = iks_new( "iq" );
+        iks_insert_attrib( x, "type", "result" );
+        iks_insert_attrib( x, "id", pak->id );
+        iks_insert_attrib( x, "to", pak->from->full );
+        iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+        iks* y = iks_insert( x, "query" );
+        iks_insert_attrib( y, "xmlns", XMLNS_DISCO_ITEMS );
+        m_parent->send( x );
+      }
+      break;
+
+    case IKS_TYPE_SET:
+      
+      break;
+
+    case IKS_TYPE_RESULT:
+      
+      break;
+
+    case IKS_TYPE_ERROR:
+      
+      break;
   }
 }
 
