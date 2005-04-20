66754435646342d78b0a8d5c5718d5be98fe9456
Jakob Schröter <js@camaya.net>
2005-04-20 12:53:11 +0000

    + disco implementation moved from jclient.{h,cpp}

--- src/jlib/disco.cpp
+++ src/jlib/disco.cpp
@@ -24,6 +24,9 @@
 Disco::Disco( JClient* parent )
   : m_parent( parent )
 {
+  addFeature( XMLNS_VERSION );
+  addFeature( XMLNS_DISCO_INFO );
+  addFeature( XMLNS_DISCO_ITEMS );
   m_parent->registerIqHandler( this, XMLNS_DISCO_INFO );
   m_parent->registerIqHandler( this, XMLNS_DISCO_ITEMS );
 }
@@ -35,12 +38,92 @@ Disco::~Disco()
 
 void Disco::handleIq( const char* xmlns, ikspak* pak )
 {
-  if( iks_strncmp( XMLNS_DISCO_INFO, xmlns, iks_strlen( XMLNS_DISCO_INFO ) ) == 0 )
+  if( iks_strncmp( XMLNS_VERSION, xmlns, iks_strlen( XMLNS_VERSION ) ) == 0 )
   {
-    
+    iks* x = iks_new( "iq" );
+    iks_insert_attrib( x, "type", "result" );
+    iks_insert_attrib( x, "to", pak->from->full );
+    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+    iks_insert_attrib( x, "id", pak->id );
+    iks* y = iks_insert( x, "query" );
+    iks_insert_attrib( y, "xmlns", XMLNS_VERSION );
+    iks* z = iks_insert( y, "name" );
+    iks_insert_cdata( z, m_versionName.c_str(), m_versionName.length() );
+    z = iks_insert( y, "version" );
+    iks_insert_cdata( z, m_versionVersion.c_str(), m_versionVersion.length() );
+    m_parent->send( x );
+  }
+  else if( iks_strncmp( XMLNS_DISCO_INFO, xmlns, iks_strlen( XMLNS_DISCO_INFO ) ) == 0 )
+  {
+    iks* x = iks_new( "iq" );
+    iks_insert_attrib( x, "type", "result" );
+    iks_insert_attrib( x, "id", pak->id );
+    iks_insert_attrib( x, "to", pak->from->full );
+    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+    iks* y = iks_insert( x, "query" );
+    iks_insert_attrib( y, "xmlns", XMLNS_DISCO_INFO );
+    iks* i = iks_insert( y, "identity" );
+    iks_insert_attrib( i, "category", m_identityCategory.c_str() );
+    iks_insert_attrib( i, "type", m_identityType.c_str() );
+    iks_insert_attrib( i, "name", m_versionName.c_str() );
+
+    StringList::const_iterator it = m_features.begin();
+    for( it; it != m_features.end(); ++it )
+    {
+      iks* z = iks_insert( y, "feature" );
+      iks_insert_attrib( z, "var", (*it).c_str() );
+    }
+    m_parent->send( x );
   }
   else if( iks_strncmp( XMLNS_DISCO_ITEMS, xmlns, iks_strlen( XMLNS_DISCO_ITEMS ) ) == 0 )
   {
-    
+    iks* x = iks_new( "iq" );
+    iks_insert_attrib( x, "type", "result" );
+    iks_insert_attrib( x, "id", pak->id );
+    iks_insert_attrib( x, "to", pak->from->full );
+    iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+    iks* y = iks_insert( x, "query" );
+    iks_insert_attrib( y, "xmlns", XMLNS_DISCO_ITEMS );
+    m_parent->send( x );
   }
 }
+
+void Disco::addFeature( const string& feature )
+{
+  m_features.push_back( feature );
+}
+
+Disco::StringList Disco::getDiscoInfo( const string& to )
+{
+  std::string id = /*m_parent->getID()*/"test";
+  iks* x = iks_make_iq( IKS_TYPE_GET, XMLNS_DISCO_INFO );
+  iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+  iks_insert_attrib( x, "to", to.c_str() );
+  iks_insert_attrib( x, "id", id.c_str() );
+  m_parent->send( x );
+//   addQueryID( to, id );
+}
+
+Disco::StringList Disco::getDiscoItems( const string& to )
+{
+  std::string id = /*m_parent->getID()*/"test";
+  iks* x = iks_make_iq( IKS_TYPE_GET, XMLNS_DISCO_ITEMS );
+  iks_insert_attrib( x, "from", m_parent->jid().c_str() );
+  iks_insert_attrib( x, "to", to.c_str() );
+  iks_insert_attrib( x, "id", id.c_str() );
+  m_parent->send( x );
+//   addQueryID( to, id );
+}
+
+void Disco::setVersion( const string& name, const string& version )
+{
+  m_versionName = name;
+  m_versionVersion = version;
+}
+
+void Disco::setIdentity( const string& category, const string& type )
+{
+  m_identityCategory = category;
+  m_identityType = type;
+}
+
--- src/jlib/disco.h
+++ src/jlib/disco.h
@@ -23,11 +23,21 @@
 
 #include "iqhandler.h"
 
+#include <string>
+#include <list>
+using namespace std;
+
 class JClient;
 
+/**
+ * This class implements JEP-0030 (Service Discovery).
+ * 
+ */
 class Disco : public IqHandler
 {
   public:
+    typedef list<string> StringList;
+
     /**
      * Constructor.
      * Creates a new Disco client that registers as IqHandler with @c JClient.
@@ -41,12 +51,68 @@ class Disco : public IqHandler
     virtual ~Disco();
 
     /**
+     * Adds a feature to the list of supported Jabber features.
+     * The list will be posted as an answer to IQ queries in the
+     * "http://jabber.org/protocol/disco#info" namespace.
+     * These IQ packets will also be forwarded to the
+     * application's IqHandler, if it listens to the disco#info namespace.
+     * You can call @ref disableDiscoInfo() to disable automatic disco#info
+     * entirely. By default, disco(very) queries are handled by the library.
+     * By default, all supported, not disabled features are announced.
+     * @param feature A feature (namespace) the host app supports.
+     */
+    void addFeature( const string& feature );
+
+    /**
+     * Queries the given JID for general infomation according to
+     * JEP-0030 (Service Discovery).
+     * @param to The destination-JID of the query.
+     * @return A list of capabilities.
+     */
+    Disco::StringList getDiscoInfo( const string& to );
+
+    /**
+     * Queries the given JID for its items according to
+     * JEP-0030 (Service Discovery).
+     * @param to The destination-JID of the query.
+     * @return A list of items.
+     */
+    Disco::StringList getDiscoItems( const string& to );
+
+    /**
+     * Sets the version of the host application using this library.
+     * The library takes care of jabber:iq:version requests. These
+     * IQ packets will not be forwarded to the IqHandlers.
+     * @param name The name to be returned to inquireing clients.
+     * @param version The version to be returned to inquireing clients.
+     */
+    void setVersion( const string& name, const string& version );
+
+    /**
+     * Sets the identity of the this entity.
+     * The library uses this information to answer disco#info requests
+     * with a correct identity.
+     * JEP-0030 requires an entity to have at least one identity. See JEP-0030
+     * for more information on categories and types.
+     * @param category The entity category of this client. Default: client
+     * @param type The type of this entity. Default: bot
+     */
+    void setIdentity( const string& category, const string& type );
+
+    /**
      * reimplemented from IqHandler.
      */
     virtual void handleIq( const char* xmlns, ikspak* pak );
 
   private:
     JClient* m_parent;
+
+    StringList m_features;
+
+    string m_versionName;
+    string m_versionVersion;
+    string m_identityCategory;
+    string m_identityType;
 
 };
 
