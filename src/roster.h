382bc244fc5bfeb46253d444d3afec0b61a45ef5
Jakob Schröter <js@camaya.net>
2005-06-29 19:49:47 +0000

    + compile fix * whitespace * FSF has a new address

--- src/roster.h
+++ src/roster.h
@@ -13,7 +13,7 @@
 
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 */
 
 
@@ -109,7 +109,7 @@ class Roster : public IqHandler, PresenceHandler, SubscriptionHandler/*, DiscoHa
     struct ItemFeatures
     {
       list<Identity> identities;
-      list<const string> features;
+//       list<const string> features;
     };
 
     typedef map<const string, ItemFeatures> FeatureMap;
