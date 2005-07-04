18d214e33831c95b5f692a47ca69108f2c659d56
Jakob Schröter <js@camaya.net>
2005-07-04 23:25:08 +0000

    + JEP-0077 result codes and field handling

--- src/registrationhandler.h
+++ src/registrationhandler.h
@@ -38,12 +38,58 @@ class RegistrationHandler
 {
   public:
     /**
+     * Possible results of a JEP-0077 operation.
+     */
+    enum resultEnum
+    {
+      REGISTRATION_SUCCESS,                 /*!< The last operation (account registration, account deletion or
+                                             * password change) was successful. */
+      REGISTRATION_NOT_ACCEPTABLE,          /*!< 406: Not all necessary information provided */
+      REGISTRATION_CONFLICT,                /*!< 409: Username alreday exists. */
+      REGISTRATION_NOT_AUTHORIZED,          /*!< Account removal: Unregistered entity waits too long before
+                                             * authentication or performs tasks other than authentication after
+                                             * registration.<br>
+                                             * Password change: The server or service does not consider the
+                                             * channel safe enough to enable a password change. */
+      REGISTRATION_BAD_REQUEST,             /*!< Account removal: The &lt;remove/&gt; element was not the only
+                                             * child element of the &lt;query/&gt; element. Should not happen
+                                             * when only gloox functions are being used.<br>
+                                             * Password change: The password change request does not contain
+                                             * complete information (both &lt;username/&gt; and
+                                             * &lt;password/&gt; are required). */
+      REGISTRATION_FORBIDDEN,               /*!< Account removal: The sender does not have sufficient
+                                             * permissions to cancel the registration. */
+      REGISTRATION_REGISTRATION_REQUIRED,   /*!< Account removal: The entity sending the remove request was
+                                             * not previously registered. */
+      REGISTRATION_UNEXPECTED_REQUEST,      /*!< Account removal: The host is an instant messaging server and
+                                             * the IQ get does not contain a 'from' address because the entity
+                                             * is not registered with the server.<br>
+                                             * Password change: The host is an instant messaging server and
+                                             * the IQ set does not contain a 'from' address because the entity
+                                             * is not registered with the server. */
+      REGISTRATION_NOT_ALLOWED              /*!< Password change: The server or service does not allow
+                                             * password changes. */
+    };
+
+    /**
      * Reimplement this function to receive results of the @ref Registration::fetchRegistrationFields()
      * function.
      * @param fields The OR'ed fields the server requires. From @ref Registration::fieldEnum.
      * @param instructions Any additional information the server sends along.
      */
     virtual void handleRegistrationFields( int fields, string instructions ) {};
+
+    /**
+     * This function is called if @ref Registration::createAccount() was called on an authenticated stream
+     * and the server lets us know about this.
+     */
+    virtual void handleAlreadyRegistered() {};
+
+    /**
+     * This funtion is called to notify about the result of an operation.
+     * @param result The result of the last operation.
+     */
+    virtual void handleResult( resultEnum result ) {};
 };
 
 #endif // REGISTRATIONHANDLER_H__
