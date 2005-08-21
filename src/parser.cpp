a39e333c15cc061f9746db05ddd264f74adf0eb9
Jakob Schröter <js@camaya.net>
2005-08-21 11:59:14 +0000

    + a parser for now it's only an abstraction of the libiksemel parser, though
    git-svn-id: svn://svn.camaya.net/gloox/trunk@466 bb60cc1b-8fe5-0310-9879-aca5d778a633

---
+++ src/parser.cpp
@@ -0,0 +1,165 @@
+/*
+  Copyright (c) 2004-2005 by Jakob Schroeter <js@camaya.net>
+  This file is part of the gloox library. http://camaya.net/gloox
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+
+
+// #include "config.h"
+
+#include "gloox.h"
+
+#include "parser.h"
+#include "clientbase.h"
+#include "tag.h"
+
+using namespace std;
+
+
+namespace gloox
+{
+
+  Parser::Parser( ClientBase *parent, const std::string& ns )
+    : m_parent( parent )
+  {
+    m_parser = iks_stream_new( (char*)ns.c_str(), this, (iksStreamHook*)streamHook );
+  }
+
+  Parser::~Parser()
+  {
+    iks_parser_delete( m_parser );
+  }
+
+  Parser::ParserState Parser::feed( const std::string& data )
+  {
+    printf( "parser received: %s\n", data.c_str() );
+    int res = iks_parse( m_parser, data.c_str(), data.length(), 0 );
+    switch( res )
+    {
+      case IKS_OK:
+        return PARSER_OK;
+        break;
+      case IKS_NOMEM:
+        return PARSER_NOMEM;
+        break;
+      case IKS_BADXML:
+        return PARSER_BADXML;
+        break;
+      case IKS_HOOK:
+        return PARSER_HOOK;
+        break;
+    }
+  }
+
+  void Parser::streamEvent( int type, iks* node )
+  {
+    if( m_parent )
+    {
+      Tag tag = convertFromIks( node );
+      m_parent->filter( type, tag );
+    }
+
+    iks_delete( node );
+  }
+
+  const Tag Parser::convertFromIks( iks *x )
+  {
+    Tag tag( iks_name( x ) );
+    if( iks_cdata( x ) )
+      tag.setCData( iks_cdata( x ) );
+
+    iks *y = iks_attrib( x );
+    while( y )
+    {
+      tag.addAttrib( iks_name( y ), iks_cdata( y ) );
+      y = iks_next( y );
+    }
+
+    iks *z = iks_first_tag( x );
+    while( z )
+    {
+      tag.addChild( convertFromIks( z ) );
+      z = iks_next_tag( z );
+    }
+
+    return tag;
+  }
+
+//   int msgHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in msgHook\n" );
+//     if( cb )
+//       cb->notifyMessageHandlers( pak->from, pak->subtype, iks_find_cdata( pak->x, "body" ) );
+//     return IKS_FILTER_EAT;
+//   }
+//
+//   int iqHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in iqHook\n" );
+//     if( cb )
+//       cb->notifyIqHandlers( pak->ns, pak );
+//     return IKS_FILTER_EAT;
+//   }
+//
+//   int presenceHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in presenceHook\n" );
+//     if( cb )
+//       cb->notifyPresenceHandlers( pak->from, pak->subtype, pak->show,
+//                                       iks_find_cdata( pak->x, "status" ) );
+//     return IKS_FILTER_EAT;
+//   }
+//
+//   int subscriptionHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in subscriptionHook\n" );
+//     if( cb )
+//       cb->notifySubscriptionHandlers( pak->from, pak->subtype, iks_find_cdata( pak->x, "status" ) );
+//     return IKS_FILTER_EAT;
+//   }
+//
+//   int bindHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in Parser::bindHook\n" );
+// //    if( cb )
+// //      cb->notifySubscriptionHandlers( pak->from, pak->subtype, iks_find_cdata( pak->x, "status" ) );
+//     return IKS_FILTER_EAT;
+//   }
+//
+//   int sessionHook( ClientBase *cb, ikspak* pak )
+//   {
+//     printf( "in Parser::sessionHook\n" );//    if( cb )
+// //      cb->notifySubscriptionHandlers( pak->from, pak->subtype, iks_find_cdata( pak->x, "status" ) );
+//     return IKS_FILTER_EAT;
+//   }
+
+  int streamHook( Parser *parser, int type, iks *node )
+  {
+    printf( "in streamHook\n" );
+    parser->streamEvent( type, node );
+    return IKS_OK;
+  }
+
+//   int logHook( ClientBase *cb, const char *data, size_t size, int is_incoming )
+//   {
+//     printf( "in logHook\n" );
+//     if( cb )
+//       cb->logEvent( data, size, is_incoming );
+//     return IKS_OK;
+//   }
+
+};
---
+++ src/parser.h
@@ -0,0 +1,86 @@
+/*
+  Copyright (c) 2004-2005 by Jakob Schroeter <js@camaya.net>
+  This file is part of the gloox library. http://camaya.net/gloox
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+*/
+
+
+
+#ifndef PARSER_H__
+#define PARSER_H__
+
+#include "gloox.h"
+
+#include <iksemel.h>
+
+#include <string>
+
+namespace gloox
+{
+
+  class ClientBase;
+  class Tag;
+
+  /**
+   * This class is an abstraction of libiksemel's XML parser.
+   * @author Jakob Schroeter <js@camaya.net>
+   * @since 0.4
+   */
+  class Parser
+  {
+    public:
+      /**
+       * Describes the return values of the parser.
+       */
+      enum ParserState
+      {
+        PARSER_OK,                     /**< Everything's alright. */
+        PARSER_NOMEM,                  /**< Memory allcation error. */
+        PARSER_BADXML,                 /**< XML parse error. */
+        PARSER_HOOK                    /**< Unknown. */
+      };
+
+      /**
+       * Constructs a new Parser object.
+       * @param parent The object to send incoming Tags to.
+       * @param ns The parser's namespace. Legacy, libiksemel-related.
+       */
+      Parser( ClientBase *parent, const std::string& ns );
+
+      /**
+       * Virtual destructor.
+       */
+      virtual ~Parser();
+
+      /**
+       * Use this function to feed the parser with more XML.
+       * @param data Raw xml to parse.
+       */
+      ParserState feed( const std::string& data );
+
+    private:
+      void streamEvent( int type, iks* node );
+      const Tag convertFromIks( iks *x );
+
+      iksparser *m_parser;
+      ClientBase *m_parent;
+
+      friend int streamHook( Parser *parser, int type, iks *node );
+  };
+
+};
+
+#endif // PARSER_H__
