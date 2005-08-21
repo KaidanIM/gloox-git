6574bcff3af1e53bd9ed1fa661266618dd0eddc9
Jakob Schröter <js@camaya.net>
2005-08-21 14:56:30 +0000

    * use Tag & Stanza * std::string

--- src/disconodehandler.h
+++ src/disconodehandler.h
@@ -25,7 +25,6 @@
 #include <list>
 #include <map>
 #include <string>
-using namespace std;
 
 namespace gloox
 {
@@ -42,21 +41,21 @@ namespace gloox
   {
     public:
       /**
-       * Holds a list of items. The first string is the item's node specifier,
-       * the second string is the items natural-language name.
+       * Holds a list of items. The first std::string is the item's node specifier,
+       * the second std::string is the items natural-language name.
        */
-      typedef map<string, string> ItemMap;
+      typedef std::map<std::string, std::string> ItemMap;
 
       /**
        * Holds a list of features.
        */
-      typedef list<string> FeatureList;
+      typedef std::list<std::string> FeatureList;
 
       /**
-       * Holds a list of identities for a given node. The first string is the
-       * category specifier, the second string is the type specifier.
+       * Holds a list of identities for a given node. The first std::string is the
+       * category specifier, the second std::string is the type specifier.
        */
-      typedef map<string, string> IdentityMap;
+      typedef std::map<std::string, std::string> IdentityMap;
 
       /**
        * In addition to @c handleDiscoNodeIdentities, this function is used to gather
@@ -65,7 +64,7 @@ namespace gloox
        * @param node The node this handler is supposed to handle.
        * @return A list of features supported by this node.
        */
-      virtual FeatureList handleDiscoNodeFeatures( const char *node ) {};
+      virtual FeatureList handleDiscoNodeFeatures( const std::string& node ) {};
 
       /**
        * In addition to @c handleDiscoNodeFeatures, this function is used to gather
@@ -74,17 +73,17 @@ namespace gloox
        * @param node The node this handler is supposed to handle.
        * @return A list of identities for this node.
        */
-      virtual IdentityMap handleDiscoNodeIdentities( const char *node ) {};
+      virtual IdentityMap handleDiscoNodeIdentities( const std::string& node ) {};
 
       /**
        * This function is used to gather more information on a specific node.
        * It is called when a disco#items query arrives with a node attribute that
-       * matches the one registered for this handler. If node is 0, items for the
+       * matches the one registered for this handler. If node is empty, items for the
        * root node (no node) shall be returned.
        * @param node The node this handler is supposed to handle.
        * @return A map of items supported by this node.
        */
-      virtual ItemMap handleDiscoNodeItems( const char *node = 0 ) {};
+      virtual ItemMap handleDiscoNodeItems( const std::string& node = "" ) {};
 
   };
 
