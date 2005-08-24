4f813f99db623513a02d9766819e90b57d7e7f0e
Jakob Schröter <js@camaya.net>
2005-08-24 09:31:00 +0000

    * prepare stanza for being instantiated by the parser

--- src/stanza.cpp
+++ src/stanza.cpp
@@ -13,91 +13,102 @@
 namespace gloox
 {
 
-  Stanza::Stanza( const Tag *tag )
-    : m_show( PRESENCE_UNKNOWN )
+  Stanza::Stanza( const std::string& name, const std::string& cdata )
+  : Tag( name, cdata )
+  {
+  }
+
+  Stanza::Stanza( Tag *tag )
+    : Tag( tag->name() ),
+      m_show( PRESENCE_UNKNOWN )
   {
     m_name = tag->name();
-    m_attribs = ( const_cast<Tag*>( tag ) )->attributes();
+    m_attribs = tag->attributes();
     m_cdata = tag->cdata();
-    m_children = ( const_cast<Tag*>( tag ) )->children();
+    m_children = tag->children();
+//     m_parent = tag->parent();
+    init();
+  }
 
-    m_from.setJID( tag->findAttribute( "from" ) );
-    m_to.setJID( tag->findAttribute( "to" ) );
-    m_id = tag->findAttribute( "id" );
+  void Stanza::init()
+  {
+    m_from.setJID( findAttribute( "from" ) );
+    m_to.setJID( findAttribute( "to" ) );
+    m_id = findAttribute( "id" );
 
     if( m_name == "iq" )
     {
       m_type = STANZA_IQ;
-      if( tag->hasAttribute( "type", "get" ) )
+      if( hasAttribute( "type", "get" ) )
         m_subtype = STANZA_IQ_GET;
-      else if( tag->hasAttribute( "type", "set" ) )
+      else if( hasAttribute( "type", "set" ) )
         m_subtype = STANZA_IQ_SET;
-      else if( tag->hasAttribute( "type", "result" ) )
+      else if( hasAttribute( "type", "result" ) )
         m_subtype = STANZA_IQ_RESULT;
-      else if( tag->hasAttribute( "type", "error" ) )
+      else if( hasAttribute( "type", "error" ) )
         m_subtype = STANZA_IQ_ERROR;
       else
         m_subtype = STANZA_SUB_UNDEFINED;
 
       Tag *t = findChildWithAttrib( "xmlns" );
       if( t )
-        m_xmlns = findAttribute( "xmlns" );
+        m_xmlns = t->findAttribute( "xmlns" );
     }
     else if( m_name == "message" )
     {
       m_type = STANZA_MESSAGE;
-      if( tag->hasAttribute( "type", "chat" ) )
+      if( hasAttribute( "type", "chat" ) )
         m_subtype = STANZA_MESSAGE_CHAT;
-      else if( tag->hasAttribute( "type", "error" ) )
+      else if( hasAttribute( "type", "error" ) )
         m_subtype = STANZA_MESSAGE_ERROR;
-      else if( tag->hasAttribute( "type", "headline" ) )
+      else if( hasAttribute( "type", "headline" ) )
         m_subtype = STANZA_MESSAGE_HEADLINE;
-      else if( tag->hasAttribute( "type", "groupchat" ) )
+      else if( hasAttribute( "type", "groupchat" ) )
         m_subtype = STANZA_MESSAGE_GROUPCHAT;
-      else if( tag->hasAttribute( "type", "normal" ) )
+      else if( hasAttribute( "type", "normal" ) )
         m_subtype = STANZA_MESSAGE_NORMAL;
       else
         m_subtype = STANZA_SUB_UNDEFINED;
     }
     else if( m_name == "presence" )
     {
-      if( tag->hasAttribute( "type", "subscribe" ) )
+      if( hasAttribute( "type", "subscribe" ) )
       {
         m_type = STANZA_S10N;
         m_subtype = STANZA_S10N_SUBSCRIBE;
       }
-      else if( tag->hasAttribute( "type", "subscribed" ) )
+      else if( hasAttribute( "type", "subscribed" ) )
       {
         m_type = STANZA_S10N;
         m_subtype = STANZA_S10N_SUBSCRIBED;
       }
-      else if( tag->hasAttribute( "type", "unsubscribe" ) )
+      else if( hasAttribute( "type", "unsubscribe" ) )
       {
         m_type = STANZA_S10N;
         m_subtype = STANZA_S10N_UNSUBSCRIBE;
       }
-      else if( tag->hasAttribute( "type", "unsubscribed" ) )
+      else if( hasAttribute( "type", "unsubscribed" ) )
       {
         m_type = STANZA_S10N;
         m_subtype = STANZA_S10N_UNSUBSCRIBED;
       }
-      else if( tag->hasAttribute( "type", "unavailable" ) )
+      else if( hasAttribute( "type", "unavailable" ) )
       {
         m_type = STANZA_PRESENCE;
         m_subtype = STANZA_PRES_UNAVAILABLE;
         m_show = PRESENCE_UNAVAILABLE;
       }
-      else if( tag->hasAttribute( "type", "probe" ) )
+      else if( hasAttribute( "type", "probe" ) )
       {
         m_type = STANZA_PRESENCE;
         m_subtype = STANZA_PRES_PROBE;
       }
-      else if( tag->hasAttribute( "type", "error" ) )
+      else if( hasAttribute( "type", "error" ) )
       {
         m_type = STANZA_PRESENCE;
         m_subtype = STANZA_PRES_ERROR;
       }
-      else if( !tag->hasAttribute( "type" ) )
+      else if( !hasAttribute( "type" ) )
       {
         m_type = STANZA_PRESENCE;
         m_subtype = STANZA_PRES_AVAILABLE;
@@ -137,4 +148,4 @@ namespace gloox
     }
   }
 
-}
+};
