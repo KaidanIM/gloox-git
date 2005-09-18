0d91ce3346d7e3baece3b02e2e7434a70f67a8f0
Jakob Schröter <js@camaya.net>
2005-09-18 16:55:04 +0000

    + results of private xml operations

--- src/privatexmlhandler.h
+++ src/privatexmlhandler.h
@@ -15,15 +15,13 @@
 #ifndef PRIVATEXMLHANDLER_H__
 #define PRIVATEXMLHANDLER_H__
 
-#include <iksemel.h>
-
 #include <string>
 
 namespace gloox
 {
 
   /**
-   * A virtual interface which can be reimplemented to receive data stored in private XML.
+   * A virtual interface which can be reimplemented to store and receive private XML data.
    * Derived classes can be registered as PrivateXMLHandlers with the PrivateXML object.
    * Upon an incoming PrivateXML packet @ref handlePrivateXML() will be called.
    * @author Jakob Schroeter <js@camaya.net>
@@ -32,14 +30,32 @@ namespace gloox
   {
     public:
       /**
+       * Describes the possible results of a 'store' or 'request' operation.
+       */
+      enum PrivateXMLResult
+      {
+        PXML_STORE_OK,              /**< Storing was successful. */
+        PXML_STORE_ERROR,           /**< An error occurred while storing data in Private XML. */
+        PXML_REQUEST_ERROR,         /**< An error occurred while requesting Private XML. */
+      };
+
+      /**
        * Reimplement this function to receive the private XML that was requested earlier using
        * @c PrivateXML::requestXML().
        * @param tag The tag of the storage packet.
        * @param xmlns The XML namespace of the storage packet.
        * @param xml The private xml, i.e. the first child of the &lt;query&gt; tag.
        */
-      virtual void handlePrivateXML( const std::string& tag, const std::string& xmlns, Tag *xml ) {};
+      virtual void handlePrivateXML( const std::string& tag, const std::string& xmlns, Tag *xml ) = 0;
 
+      /**
+       * This function is called to notify about the result of a 'store' or 'request' operation
+       * (successful requests are announced by means of @ref handlePrivateXML()).
+       * @param uid The ID of the query.
+       * @param result The result of the operation.
+       * @since 0.7
+       */
+      virtual void handlePrivateXMLResult( const std::string uid, PrivateXMLResult result ) = 0;
   };
 
 };
