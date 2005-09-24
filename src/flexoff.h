ff5a8bc71717e387aaef5714a07f5b818c566ebe
Jakob Schröter <js@camaya.net>
2005-09-24 14:21:14 +0000

    + 60% complete implementation of JEP-0013
    git-svn-id: svn://svn.camaya.net/gloox/trunk@748 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/flexoff.h
+++ src/flexoff.h
@@ -14,18 +14,22 @@
 #ifndef FLEXOFF_H__
 #define FLEXOFF_H__
 
+#include "clientbase.h"
 #include "disco.h"
 #include "discohandler.h"
+#include "flexoffhandler.h"
+#include "iqhandler.h"
 
 namespace gloox
 {
 
   /**
    * An implementation of JEP-0013 (Flexible Offline Message Retrieval).
+   * Use the FlexibleOfflineHandler to receive results.
    * @author Jakob Schroeter <js@camaya.net>
    * @since 0.7
    */
-  class FlexibleOffline : public DiscoHandler
+  class FlexibleOffline : public DiscoHandler, IqHandler
   {
     public:
       /**
@@ -33,7 +37,7 @@ namespace gloox
        * @param parent The ClientBase to use for communication.
        * @param disco The disco object to use.
        */
-      FlexibleOffline();
+      FlexibleOffline( ClientBase *parent, Disco *disco );
 
       /**
        * Virtual Destructor.
@@ -43,11 +47,12 @@ namespace gloox
       /**
        * Initiates querying the server for Flexible Offline Message Retrieval-support.
        * The result is announced through the FlexibleOfflineHandler.
+       * An application could cache the result on a per-server basis to eliminate the associated delay.
        */
       void checkSupport();
 
       /**
-       * Initiates asking the server for the number of stored offline messages.
+       * Asks the server for the number of stored offline messages.
        * The result is announced through the FlexibleOfflineHandler.
        */
       void getMsgCount();
@@ -61,17 +66,57 @@ namespace gloox
       /**
        * Initiates fetching of one or more specific messages, or all messages.
        * The result is announced through the FlexibleOfflineHandler.
+       * If the list of message nodes contains one or more nodes, the corresponding messages are
+       * fetched. If the list is empty all messages are fetched (&lt;fetch&gt;).
+       * @param msgs A list of message nodes to fetch.
        */
-      void fetchMessages();
+      void fetchMessages( const StringList& msgs );
 
       /**
        * Initiates removing of one or more specific messages, or all messages.
        * The result is announced through the FlexibleOfflineHandler.
+       * If the list of message nodes contains one or more nodes, the corresponding messages are
+       * removed. If the list is empty all messages are removed (&lt;purge&gt;).
        */
-      void removeMessages();
+      void removeMessages( const StringList& msgs );
+
+      /**
+       * Registers a FlexibleOfflineHandler as object that receives results of JEP-0013 queries.
+       * Only one Handler at a time is possible.
+       * @param foh The Handler object to register.
+       */
+      void registerFlexibleOfflineHandler( FlexibleOfflineHandler *foh );
+
+      /**
+       * Removes the registered handler.
+       */
+      void removeFlexibleOfflineHandler();
+
+      // reimplemented from DiscoHandler
+      virtual void handleDiscoInfoResult( Stanza *stanza, int context );
+
+      // reimplemented from DiscoHandler
+      virtual void handleDiscoItemsResult( Stanza *stanza, int contex );
+
+      // reimplemented from IqHandler
+      virtual bool handleIqID( Stanza *stanza, int context );
+
+      // reimplemented from IqHandler
+      virtual bool handleIq( Stanza *stanza ) { return false; };
 
     private:
+      enum FOContext
+      {
+        FO_CHECK_SUPPORT,
+        FO_REQUEST_NUM,
+        FO_REQUEST_HEADERS,
+        FO_REQUEST_MSGS,
+        FO_REMOVE_MSGS,
+      };
 
+      ClientBase *m_parent;
+      Disco *m_disco;
+      FlexibleOfflineHandler *m_flexibleOfflineHandler;
   };
 
 };
