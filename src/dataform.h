9ed92fe198a1561b380e146d15f2408cc168499b
Jakob Schröter <js@camaya.net>
2005-09-25 00:56:53 +0000

    - typo

--- src/dataform.h
+++ src/dataform.h
@@ -25,7 +25,7 @@ namespace gloox
   class Tag;
 
   /**
-   * An abstarction of a JEP-0004 Data Form.
+   * An abstraction of a JEP-0004 Data Form.
    * @author Jakob Schroeter <js@camaya.net>
    * @since 0.7
    * @note This implementation lacks support for Section 3.4 of JEP-0004 (Multiple Items in Form Results),
