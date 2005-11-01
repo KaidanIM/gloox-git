d7d0a0b0c5de606eebf2595230212ed53bb293d5
Jakob Schröter <js@camaya.net>
2005-11-01 23:22:44 +0000

    ** 0.7 final **

--- src/gloox.h
+++ src/gloox.h
@@ -243,7 +243,7 @@
 
 #define XMPP_STREAM_VERSION_MAJOR  "1"
 #define XMPP_STREAM_VERSION_MINOR  "0"
-#define GLOOX_VERSION              "0.7-pre4.1"
+#define GLOOX_VERSION              "0.7"
 
 /**
  * @brief The namespace for the gloox library.
