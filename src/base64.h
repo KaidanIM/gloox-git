d155203c32521a36fbcbbabeec29edb7927ace79
Jakob Schröter <js@camaya.net>
2006-01-13 00:11:05 +0000

    + Base64 decoding + use own Base64 decoding
    git-svn-id: svn://svn.camaya.net/gloox/trunk@1055 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/base64.h
+++ src/base64.h
@@ -32,25 +32,23 @@ namespace gloox
 
     public:
       /**
-       *
+       * Encodes the input according to RFC 3548.
+       * @param input The data to encode.
+       * @return The encoded string.
        */
       static const std::string encode64( const std::string& input );
 
       /**
-       *
+       * Decodes the input according to RFC 3548.
+       * @param input The encoded data.
+       * @return The decoded data.
        */
       static const std::string decode64( const std::string& input );
 
-      /**
-       *
-       */
     private:
       static const std::string alphabet64;
-
+      static const std::string::size_type table64[];
   };
-
-//   const std::string Base64::alphabet32 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-//   const std::string Base64::alphabet16 = "0123456789ABCDEF";
 
 }
 
