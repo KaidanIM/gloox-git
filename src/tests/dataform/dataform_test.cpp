735584bedc786a7e6dd6093ccad766980599f1fc
Jakob Schröter <js@camaya.net>
2006-07-24 22:48:07 +0000

    + DataFormField tests + more DataForm test

--- src/tests/dataform/dataform_test.cpp
+++ src/tests/dataform/dataform_test.cpp
@@ -1,4 +1,5 @@
 #include "../../dataform.h"
+#include "../../tag.h"
 using namespace gloox;
 
 #include <stdio.h>
@@ -7,7 +8,7 @@ using namespace gloox;
 
 int main( int /*argc*/, char* /*argv[]*/ )
 {
-  bool fail = false;
+  int fail = 0;
   std::string name;
   DataForm *f;
 
@@ -16,7 +17,7 @@ int main( int /*argc*/, char* /*argv[]*/ )
   f = new DataForm();
   if( f->type() != DataForm::FormTypeInvalid )
   {
-    fail = true;
+    ++fail;
     printf( "test '%s' failed\n", name.c_str() );
   }
   delete f;
@@ -27,7 +28,7 @@ int main( int /*argc*/, char* /*argv[]*/ )
   f = new DataForm();
   if( f->tag() )
   {
-    fail = true;
+    ++fail;
     printf( "test '%s' failed\n", name.c_str() );
   }
   delete f;
@@ -40,7 +41,7 @@ int main( int /*argc*/, char* /*argv[]*/ )
   f->setTitle( title );
   if( f->title() != title )
   {
-    fail = true;
+    ++fail;
     printf( "test '%s' failed\n", name.c_str() );
   }
   delete f;
@@ -56,7 +57,69 @@ int main( int /*argc*/, char* /*argv[]*/ )
   f->setInstructions( instructions );
   if( f->instructions() != instructions )
   {
-    fail = true;
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  delete f;
+  f = 0;
+
+  // -------
+  name = "form type, title, instructions";
+  // using StringList instructions from previous test case
+  // using std::string title from pre-previous test case
+  f = new DataForm( DataForm::FormTypeForm, instructions, title );
+  if( f->instructions() != instructions )
+  {
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  if( f->title() != title )
+  {
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  if( f->type() != DataForm::FormTypeForm )
+  {
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  delete f;
+  f = 0;
+
+  // -------
+  name = "parse empty Tag (ctor)";
+  Tag *t = new Tag();
+  f = new DataForm( t );
+  delete t;
+  if( f->type() != DataForm::FormTypeInvalid )
+  {
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  delete f;
+  f = 0;
+
+  // -------
+  name = "parse() empty Tag";
+  f = new DataForm();
+  t = new Tag();
+  f->parse( t );
+  delete t;
+  if( f->type() != DataForm::FormTypeInvalid )
+  {
+    ++fail;
+    printf( "test '%s' failed\n", name.c_str() );
+  }
+  delete f;
+  f = 0;
+
+  // -------
+  name = "parse 0";
+  f = new DataForm();
+  f->parse( 0 );
+  if( f->type() != DataForm::FormTypeInvalid )
+  {
+    ++fail;
     printf( "test '%s' failed\n", name.c_str() );
   }
   delete f;
@@ -64,13 +127,16 @@ int main( int /*argc*/, char* /*argv[]*/ )
 
 
 
-  if( !fail )
+
+
+  if( fail == 0 )
   {
     printf( "All tests passed\n" );
     return 0;
   }
   else
   {
+    printf( "%d test(s) failed\n", fail );
     return 1;
   }
 
