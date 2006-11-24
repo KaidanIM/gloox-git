bd37c0163777d7edbc78f87a5bcf90271c32ed4e
Jakob Schröter <js@camaya.net>
2006-11-24 01:42:05 +0000

    + working MD5 implementation
    git-svn-id: svn://svn.camaya.net/gloox/trunk@1540 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/md5.h
+++ src/md5.h
@@ -1,75 +1,118 @@
 /*
-  Copyright (c) 2005-2006 by Jakob Schroeter <js@camaya.net>
-  This file is part of the gloox library. http://camaya.net/gloox
+  Copyright (C) 1999, 2002 Aladdin Enterprises.  All rights reserved.
 
-  This software is distributed under a license. The full license
-  agreement can be found in the file LICENSE in this distribution.
-  This software may not be copied, modified, sold or distributed
-  other than expressed in the named license agreement.
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
 
-  This software is distributed without any warranty.
-*/
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
 
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  L. Peter Deutsch
+  ghost@aladdin.com
+
+ */
+/* $Id: md5.h,v 1.4 2002/04/13 19:20:28 lpd Exp $ */
+/*
+  Independent implementation of MD5 (RFC 1321).
+
+  This code implements the MD5 Algorithm defined in RFC 1321, whose
+  text is available at
+	http://www.ietf.org/rfc/rfc1321.txt
+  The code is derived from the text of the RFC, including the test suite
+  (section A.5) but excluding the rest of Appendix A.  It does not include
+  any code or documentation that is identified in the RFC as being
+  copyrighted.
+
+  The original and principal author of md5.h is L. Peter Deutsch
+  <ghost@aladdin.com>.  Other authors are noted in the change history
+  that follows (in reverse chronological order):
+
+  2002-04-13 lpd Removed support for non-ANSI compilers; removed
+	references to Ghostscript; clarified derivation from RFC 1321;
+	now handles byte order either statically or dynamically.
+  1999-11-04 lpd Edited comments slightly for automatic TOC extraction.
+  1999-10-18 lpd Fixed typo in header comment (ansi2knr rather than md5);
+	added conditionalization for C++ compilation from Martin
+	Purschke <purschke@bnl.gov>.
+  1999-05-03 lpd Original version.
+ */
 
 #ifndef MD5_H__
 #define MD5_H__
-
-#include "gloox.h"
-
-#include <iksemel.h>
 
 #include <string>
 
 namespace gloox
 {
 
-  /**
-   * @brief An implementation of the MD5 Message-Digest Algorithm (RFC 1321)
-   *
-   * @author Jakob Schroeter <js@camaya.net>
-   * @since 0.8
-   */
-  class GLOOX_API MD5
+  class MD5
   {
     public:
       /**
-       * Contructs a new MD5 state object.
+       *
        */
       MD5();
 
       /**
-       * Virtual destructor.
+       * Virtual Destructor.
        */
       virtual ~MD5();
 
       /**
-       * Feeds the algorithm.
+       *
        */
-      void feed( const std::string& text, bool finalize );
+      void feed( const unsigned char *data, int bytes );
 
       /**
-       * Returns the binary hash.
+       *
        */
-      const std::string hash();
+      void feed( const std::string& data );
 
       /**
-       * Returns a prettyfied (hex) version of the hash.
+       *
        */
-      const std::string pretty();
+      void finalize();
 
       /**
-       * Resets the MD5 object.
+       *
+       */
+      const std::string hex();
+
+      /**
+       *
+       */
+      const std::string binary();
+
+      /**
+       *
        */
       void reset();
 
     private:
-      iksmd5 *m_md5;
-      int funcF( int x, int y, int z );
-      int funcG( int x, int y, int z );
-      int funcH( int x, int y, int z );
-      int funcI( int x, int y, int z );
+      struct MD5State
+      {
+          unsigned int count[2]; /* message length in bits, lsw first */
+          unsigned int abcd[4]; /* digest buffer */
+          unsigned char buf[64]; /* accumulate block */
+      } m_state;
 
-      int m_buffer[4];
+      void init();
+      void process( const unsigned char *data );
+
+      static const unsigned char pad[64];
+
+      bool m_finished;
+
   };
 
 }
