97025d50571ab330c6bb74a18ff813c9d9dcaa28
Jakob Schröter <js@camaya.net>
2007-03-08 22:29:22 +0000

    * moving strings out of the .h, avoiding an instance of each for each class including gloox.h   brings us -23% static archive file size, and -30% shared object file size (on linux). yay! Thanks to Vincent Thomasset!

--- src/gloox.h
+++ src/gloox.h
@@ -302,69 +302,68 @@
  */
 namespace gloox
 {
+  extern const std::string XMLNS_CLIENT;
+  extern const std::string XMLNS_COMPONENT_ACCEPT;
+  extern const std::string XMLNS_COMPONENT_CONNECT;
 
-  const std::string XMLNS_CLIENT            = "jabber:client";                           /**< */
-  const std::string XMLNS_COMPONENT_ACCEPT  = "jabber:component:accept";                 /**< */
-  const std::string XMLNS_COMPONENT_CONNECT = "jabber:component:connect";                /**< */
+  extern const std::string XMLNS_DISCO_INFO;
+  extern const std::string XMLNS_DISCO_ITEMS;
+  extern const std::string XMLNS_ADHOC_COMMANDS;
+  extern const std::string XMLNS_COMPRESSION;
+  extern const std::string XMLNS_OFFLINE;
+  extern const std::string XMLNS_CHAT_STATES;
+  extern const std::string XMLNS_AMP;
+  extern const std::string XMLNS_IBB;
+  extern const std::string XMLNS_FEATURE_NEG;
+  extern const std::string XMLNS_CHATNEG;
+  extern const std::string XMLNS_XHTML_IM;
+  extern const std::string XMLNS_DELAY;
+  extern const std::string XMLNS_ROSTER;
+  extern const std::string XMLNS_VERSION;
+  extern const std::string XMLNS_REGISTER;
+  extern const std::string XMLNS_PRIVACY;
+  extern const std::string XMLNS_AUTH;
+  extern const std::string XMLNS_PRIVATE_XML;
+  extern const std::string XMLNS_LAST;
+  extern const std::string XMLNS_SEARCH;
+  extern const std::string XMLNS_IQ_OOB;
+  extern const std::string XMLNS_X_DATA;
+  extern const std::string XMLNS_X_EVENT;
+  extern const std::string XMLNS_X_OOB;
+  extern const std::string XMLNS_X_DELAY;
+  extern const std::string XMLNS_X_GPGSIGNED;
+  extern const std::string XMLNS_X_GPGENCRYPTED;
+  extern const std::string XMLNS_VCARD_TEMP;
+  extern const std::string XMLNS_X_VCARD_UPDATE;
+  extern const std::string XMLNS_BOOKMARKS;
+  extern const std::string XMLNS_ANNOTATIONS;
+  extern const std::string XMLNS_ROSTER_DELIMITER;
 
-  const std::string XMLNS_DISCO_INFO        = "http://jabber.org/protocol/disco#info";   /**< */
-  const std::string XMLNS_DISCO_ITEMS       = "http://jabber.org/protocol/disco#items";  /**< */
-  const std::string XMLNS_ADHOC_COMMANDS    = "http://jabber.org/protocol/commands";     /**< */
-  const std::string XMLNS_COMPRESSION       = "http://jabber.org/protocol/compress";     /**< */
-  const std::string XMLNS_OFFLINE           = "http://jabber.org/protocol/offline";      /**< */
-  const std::string XMLNS_CHAT_STATES       = "http://jabber.org/protocol/chatstates";   /**< */
-  const std::string XMLNS_AMP               = "http://jabber.org/protocol/amp";          /**< */
-  const std::string XMLNS_IBB               = "http://jabber.org/protocol/ibb";          /**< */
-  const std::string XMLNS_FEATURE_NEG       = "http://jabber.org/protocol/feature-neg";  /**< */
-  const std::string XMLNS_CHATNEG           = "http://jabber.org/protocol/chatneg";      /**< */
-  const std::string XMLNS_XHTML_IM          = "http://jabber.org/protocol/xhtml-im";     /**< */
-  const std::string XMLNS_DELAY             = "http://www.xmpp.org/extensions/xep-0203.html#ns";
-  const std::string XMLNS_ROSTER            = "jabber:iq:roster";                        /**< */
-  const std::string XMLNS_VERSION           = "jabber:iq:version";                       /**< */
-  const std::string XMLNS_REGISTER          = "jabber:iq:register";                      /**< */
-  const std::string XMLNS_PRIVACY           = "jabber:iq:privacy";                       /**< */
-  const std::string XMLNS_AUTH              = "jabber:iq:auth";                          /**< */
-  const std::string XMLNS_PRIVATE_XML       = "jabber:iq:private";                       /**< */
-  const std::string XMLNS_LAST              = "jabber:iq:last";                          /**< */
-  const std::string XMLNS_SEARCH            = "jabber:iq:search";                        /**< */
-  const std::string XMLNS_IQ_OOB            = "jabber:iq:oob";                           /**< */
-  const std::string XMLNS_X_DATA            = "jabber:x:data";                           /**< */
-  const std::string XMLNS_X_EVENT           = "jabber:x:event";                          /**< */
-  const std::string XMLNS_X_OOB             = "jabber:x:oob";                            /**< */
-  const std::string XMLNS_X_DELAY           = "jabber:x:delay";                          /**< */
-  const std::string XMLNS_X_GPGSIGNED       = "jabber:x:signed";                         /**< */
-  const std::string XMLNS_X_GPGENCRYPTED    = "jabber:x:encrypted";                      /**< */
-  const std::string XMLNS_VCARD_TEMP        = "vcard-temp";                              /**< */
-  const std::string XMLNS_X_VCARD_UPDATE    = "vcard-temp:x:update";                     /**< */
-  const std::string XMLNS_BOOKMARKS         = "storage:bookmarks";                       /**< */
-  const std::string XMLNS_ANNOTATIONS       = "storage:rosternotes";                     /**< */
-  const std::string XMLNS_ROSTER_DELIMITER  = "roster:delimiter";                        /**< */
+  extern const std::string XMLNS_XMPP_PING;
 
-  const std::string XMLNS_XMPP_PING         = "urn:xmpp:ping";                           /**< */
+  extern const std::string XMLNS_MUC;
+  extern const std::string XMLNS_MUC_USER;
+  extern const std::string XMLNS_MUC_ADMIN;
+  extern const std::string XMLNS_MUC_UNIQUE;
+  extern const std::string XMLNS_MUC_OWNER;
+  extern const std::string XMLNS_MUC_ROOMINFO;
+  extern const std::string XMLNS_MUC_ROOMS;
+  extern const std::string XMLNS_MUC_REQUEST;
 
-  const std::string XMLNS_MUC               = "http://jabber.org/protocol/muc";          /**< */
-  const std::string XMLNS_MUC_USER          = "http://jabber.org/protocol/muc#user";     /**< */
-  const std::string XMLNS_MUC_ADMIN         = "http://jabber.org/protocol/muc#admin";    /**< */
-  const std::string XMLNS_MUC_UNIQUE        = "http://jabber.org/protocol/muc#unique";   /**< */
-  const std::string XMLNS_MUC_OWNER         = "http://jabber.org/protocol/muc#owner";    /**< */
-  const std::string XMLNS_MUC_ROOMINFO      = "http://jabber.org/protocol/muc#roominfo"; /**< */
-  const std::string XMLNS_MUC_ROOMS         = "http://jabber.org/protocol/muc#rooms";    /**< */
-  const std::string XMLNS_MUC_REQUEST       = "http://jabber.org/protocol/muc#request";  /**< */
+  extern const std::string XMLNS_XMPP_STREAM;
+  extern const std::string XMLNS_XMPP_STANZAS;
+  extern const std::string XMLNS_STREAM_TLS;
+  extern const std::string XMLNS_STREAM_SASL;
+  extern const std::string XMLNS_STREAM_BIND;
+  extern const std::string XMLNS_STREAM_SESSION;
+  extern const std::string XMLNS_STREAM_IQAUTH;
+  extern const std::string XMLNS_STREAM_IQREGISTER;
+  extern const std::string XMLNS_STREAM_ACK;
+  extern const std::string XMLNS_STREAM_COMPRESS;
 
-  const std::string XMLNS_XMPP_STREAM       = "urn:ietf:params:xml:ns:xmpp-streams";     /**< */
-  const std::string XMLNS_XMPP_STANZAS      = "urn:ietf:params:xml:ns:xmpp-stanzas";     /**< */
-  const std::string XMLNS_STREAM_TLS        = "urn:ietf:params:xml:ns:xmpp-tls";         /**< */
-  const std::string XMLNS_STREAM_SASL       = "urn:ietf:params:xml:ns:xmpp-sasl";        /**< */
-  const std::string XMLNS_STREAM_BIND       = "urn:ietf:params:xml:ns:xmpp-bind";        /**< */
-  const std::string XMLNS_STREAM_SESSION    = "urn:ietf:params:xml:ns:xmpp-session";     /**< */
-  const std::string XMLNS_STREAM_IQAUTH     = "http://jabber.org/features/iq-auth";      /**< */
-  const std::string XMLNS_STREAM_IQREGISTER = "http://jabber.org/features/iq-register";  /**< */
-  const std::string XMLNS_STREAM_ACK        = "http://jabber.org/features/ack";          /**< */
-  const std::string XMLNS_STREAM_COMPRESS   = "http://jabber.org/features/compress";     /**< */
-
-  const std::string XMPP_STREAM_VERSION_MAJOR = "1";                                     /**< */
-  const std::string XMPP_STREAM_VERSION_MINOR = "0";                                     /**< */
-  const std::string GLOOX_VERSION           = "0.9-pre5";                                /**< */
+  extern const std::string XMPP_STREAM_VERSION_MAJOR;
+  extern const std::string XMPP_STREAM_VERSION_MINOR;
+  extern const std::string GLOOX_VERSION;
 
   /**
    * This describes the possible states of a stream.
