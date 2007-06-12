39e3df57daad027730d80f7e7dd74f52ac8303b7
Jakob Schröter <js@camaya.net>
2007-06-12 13:16:05 +0000

    * init to zero
    git-svn-id: svn://svn.camaya.net/gloox/trunk@2372 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/prep.cpp
+++ src/prep.cpp
@@ -52,7 +52,7 @@ namespace gloox
         return std::string();
 
       std::string preppedString;
-      char* p = static_cast<char*>( malloc( s.length() ) );
+      char* p = static_cast<char*>( calloc( JID_PORTION_SIZE, sizeof( char ) ) );
       strncpy( p, s.c_str(), s.length() );
       if( stringprep( p, JID_PORTION_SIZE, (Stringprep_profile_flags)0, profile ) == STRINGPREP_OK )
         preppedString = p;
