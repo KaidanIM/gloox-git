3256cf858ad868e8ee9397696c2ce9a8d1f16bcf
Jakob Schröter <js@camaya.net>
2007-06-17 19:26:45 +0000

    + added Tag::addAttribute( const std::string&, bool )
    git-svn-id: svn://svn.camaya.net/gloox/trunk@2408 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/tag.h
+++ src/tag.h
@@ -126,12 +126,21 @@ namespace gloox
       virtual void addAttribute( const std::string& name, int value );
 
       /**
-       * Use this function to add a new attribute to the tag. The value is an @c long here.
+       * Use this function to add a new attribute to the tag. The value is a @c long here.
        * @param name The name of the attribute.
        * @param value The value of the attribute.
        * @since 0.9
        */
       virtual void addAttribute( const std::string& name, long value );
+
+      /**
+       * Use this function to add a new attribute to the tag. The value is a @c bool here.
+       * @param name The name of the attribute.
+       * @param value The value of the attribute.
+       * @since 0.9
+       */
+      virtual void addAttribute( const std::string& name, bool value )
+        { addAttribute( name, value ? "true" : "false" ); }
 
       /**
        * Sets the given attributes. Any existing attributes are lost.
