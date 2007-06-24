81abbce98d7e98a2381da393dc2910c2e8a51069
Vincent Thomasset <vthomasset@gmail.com>
2007-06-24 00:20:46 +0000

    * use lookup

--- src/dataformfield.cpp
+++ src/dataformfield.cpp
@@ -12,10 +12,17 @@
 
 #include "dataformfield.h"
 #include "dataformbase.h"
+#include "util.h"
 #include "tag.h"
 
 namespace gloox
 {
+
+  static const char * fieldTypeValues[] =
+  {
+    "boolean", "fixed", "hidden", "jid-multi", "jid-single",
+    "list-multi", "list-single", "text-multi", "text-private", "text-single"
+  };
 
   DataFormField::DataFormField( DataFormFieldType type )
     : m_type( type ), m_required( false )
@@ -35,28 +42,14 @@ namespace gloox
     if( !tag )
       return;
 
-    if( tag->hasAttribute( "type", "boolean" ) )
-      m_type = FieldTypeBoolean;
-    else if( tag->hasAttribute( "type", "fixed" ) )
-      m_type = FieldTypeFixed;
-    else if( tag->hasAttribute( "type", "hidden" ) )
-      m_type = FieldTypeHidden;
-    else if( tag->hasAttribute( "type", "jid-multi" ) )
-      m_type = FieldTypeJidMulti;
-    else if( tag->hasAttribute( "type", "jid-single" ) )
-      m_type = FieldTypeJidSingle;
-    else if( tag->hasAttribute( "type", "list-multi" ) )
-      m_type = FieldTypeListMulti;
-    else if( tag->hasAttribute( "type", "list-single" ) )
-      m_type = FieldTypeListSingle;
-    else if( tag->hasAttribute( "type", "text-multi" ) )
-      m_type = FieldTypeTextMulti;
-    else if( tag->hasAttribute( "type", "text-private" ) )
-      m_type = FieldTypeTextPrivate;
-    else if( tag->hasAttribute( "type", "text-single" ) )
-      m_type = FieldTypeTextSingle;
-    else if( !tag->hasAttribute( "type" ) && !tag->name().empty() )
-      m_type = FieldTypeNone;
+    const std::string& type = tag->findAttribute( "type" );
+    if( type.empty() )
+    {
+      if( !tag->name().empty() )
+        m_type = FieldTypeNone;
+    }
+    else
+      m_type = (DataFormFieldType)util::lookup( type, fieldTypeValues );
 
     if( tag->hasAttribute( "var" ) )
       m_name = tag->findAttribute( "var" );
@@ -99,43 +92,7 @@ namespace gloox
       return 0;
 
     Tag *field = new Tag( "field" );
-
-    switch( m_type )
-    {
-      case FieldTypeBoolean:
-        field->addAttribute( "type", "boolean" );
-        break;
-      case FieldTypeFixed:
-        field->addAttribute( "type", "fixed" );
-        break;
-      case FieldTypeHidden:
-        field->addAttribute( "type", "hidden" );
-        break;
-      case FieldTypeJidMulti:
-        field->addAttribute( "type", "jid-multi" );
-        break;
-      case FieldTypeJidSingle:
-        field->addAttribute( "type", "jid-single" );
-        break;
-      case FieldTypeListMulti:
-        field->addAttribute( "type", "list-multi" );
-        break;
-      case FieldTypeListSingle:
-        field->addAttribute( "type", "list-single" );
-        break;
-      case FieldTypeTextMulti:
-        field->addAttribute( "type", "text-multi" );
-        break;
-      case FieldTypeTextPrivate:
-        field->addAttribute( "type", "text-private" );
-        break;
-      case FieldTypeTextSingle:
-        field->addAttribute( "type", "text-single" );
-        break;
-      default:
-        break;
-    }
-
+    field->addAttribute( "type", util::lookup( m_type, fieldTypeValues ) );
     field->addAttribute( "var", m_name );
     field->addAttribute( "label", m_label );
     if( m_required )
