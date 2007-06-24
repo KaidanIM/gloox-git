7fe1e3fe9296482b927812d720fbd2209ecdf45a
Vincent Thomasset <vthomasset@gmail.com>
2007-06-24 11:52:08 +0000

    * cleanups
    git-svn-id: svn://svn.camaya.net/gloox/trunk@2522 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/base64.cpp
+++ src/base64.cpp
@@ -16,111 +16,111 @@
 namespace gloox
 {
 
-  const std::string Base64::alphabet64( "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" );
-  const char Base64::pad = '=';
-
-  const std::string::size_type Base64::np = std::string::npos;
-  const std::string::size_type Base64::table64[] =
+  namespace Base64
   {
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, 62, np, np, np, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, np, np,
-    np, np, np, np, np,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, np, np, np, np, np, np, 26, 27, 28,
-    29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-    49, 50, 51, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np,
-    np, np, np, np, np, np, np, np, np, np, np, np, np, np, np, np
-  };
 
-  const std::string Base64::encode64( const std::string& input )
-  {
-    std::string encoded;
-    char c;
-    const std::string::size_type length = input.length();
-
-    encoded.reserve( length * 2 );
-
-    for( std::string::size_type i = 0; i < length; ++i )
+    static const std::string alphabet64( "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" );
+    static const char pad = '=';
+    static const char np  = (char)std::string::npos;
+    static char table64vals[] =
     {
-      c = ( input[i] >> 2 ) & 0x3f;
-      encoded.append( 1, alphabet64[c] );
+      62, np, np, np, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, np, np, np, np, np,
+      np, np,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17,
+      18, 19, 20, 21, 22, 23, 24, 25, np, np, np, np, np, np, 26, 27, 28, 29, 30, 31,
+      32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+    };
 
-      c = ( input[i] << 4 ) & 0x3f;
-      if( ++i < length )
-        c |= ( ( input[i] >> 4 ) & 0x0f );
-      encoded.append( 1, alphabet64[c] );
+    inline char table64( unsigned char c )
+    {
+      return ( c < 43 || c > 122 ) ? np : table64vals[c-43];
+    }
 
-      if( i < length )
+    const std::string encode64( const std::string& input )
+    {
+      std::string encoded;
+      char c;
+      const std::string::size_type length = input.length();
+
+      encoded.reserve( length * 2 );
+
+      for( std::string::size_type i = 0; i < length; ++i )
       {
-        c = ( input[i] << 2 ) & 0x3c;
+        c = ( input[i] >> 2 ) & 0x3f;
+        encoded += alphabet64[c];
+
+        c = ( input[i] << 4 ) & 0x3f;
         if( ++i < length )
-          c |= ( input[i] >> 6 ) & 0x03;
-        encoded.append( 1, alphabet64[c] );
-      }
-      else
-      {
-        ++i;
-        encoded.append( 1, pad );
+          c |= ( ( input[i] >> 4 ) & 0x0f );
+        encoded += alphabet64[c];
+
+        if( i < length )
+        {
+          c = ( input[i] << 2 ) & 0x3c;
+          if( ++i < length )
+            c |= ( input[i] >> 6 ) & 0x03;
+          encoded += alphabet64[c];
+        }
+        else
+        {
+          ++i;
+          encoded += pad;
+        }
+
+        if( i < length )
+        {
+          c = input[i] & 0x3f;
+          encoded += alphabet64[c];
+        }
+        else
+        {
+          encoded += pad;
+        }
       }
 
-      if( i < length )
-      {
-        c = input[i] & 0x3f;
-        encoded.append( 1, alphabet64[c] );
-      }
-      else
-      {
-        encoded.append( 1, pad );
-      }
+      return encoded;
     }
 
-    return encoded;
-  }
-
-  const std::string Base64::decode64( const std::string& input )
-  {
-    char c, d;
-    const std::string::size_type length = input.length();
-    std::string decoded;
-
-    decoded.reserve( length );
-
-    for( std::string::size_type i = 0; i < length; ++i )
+    const std::string decode64( const std::string& input )
     {
-      c = (char)table64[(unsigned char)input[i]];
-      ++i;
-      d = (char)table64[(unsigned char)input[i]];
-      c = ( c << 2 ) | ( ( d >> 4 ) & 0x3 );
-      decoded.append( 1, c );
-      if( ++i < length )
-      {
-        c = input[i];
-        if( pad == c )
-          break;
+      char c, d;
+      const std::string::size_type length = input.length();
+      std::string decoded;
 
-        c = (char)table64[(unsigned char)input[i]];
-        d = ( ( d << 4 ) & 0xf0 ) | ( ( c >> 2 ) & 0xf );
-        decoded.append( 1, d );
+      decoded.reserve( length );
+
+      for( std::string::size_type i = 0; i < length; ++i )
+      {
+        c = table64(input[i]);
+        ++i;
+        d = table64(input[i]);
+        c = ( c << 2 ) | ( ( d >> 4 ) & 0x3 );
+        decoded += c;
+        if( ++i < length )
+        {
+          c = input[i];
+          if( pad == c )
+            break;
+
+          c = table64(input[i]);
+          d = ( ( d << 4 ) & 0xf0 ) | ( ( c >> 2 ) & 0xf );
+          decoded += d;
+        }
+
+        if( ++i < length )
+        {
+          d = input[i];
+          if( pad == d )
+            break;
+
+          d = table64(input[i]);
+          c = ( ( c << 6 ) & 0xc0 ) | d;
+          decoded += c;
+        }
       }
 
-      if( ++i < length )
-      {
-        d = input[i];
-        if( pad == d )
-          break;
-
-        d = (char)table64[(unsigned char)input[i]];
-        c = ( ( c << 6 ) & 0xc0 ) | d;
-        decoded.append( 1, c );
-      }
+      return decoded;
     }
 
-    return decoded;
   }
 
 }
