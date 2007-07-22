cccc2b80800051895817b3ba12e90ea444aea1ce
Vincent Thomasset <vthomasset@gmail.com>
2007-07-22 20:06:52 +0000

    doc update
    git-svn-id: svn://svn.camaya.net/gloox/trunk@2686 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/pubsubdiscohandler.h
+++ src/pubsubdiscohandler.h
@@ -24,23 +24,36 @@ namespace gloox
   namespace PubSub
   {
 
+    /**
+     * Handler used for receiving results of PubSub services Disco queries.
+     *
+     * @author Vincent Thomasset
+     */
     class DiscoHandler
     {
 
       public:
 
         /**
-         *
+         * Default virtual destructor.
          */
         virtual ~DiscoHandler() {}
 
         /**
-         *
+         * Receives results for a PubSub service info query.
+         * @param service JID of the queried service.
+         * @param features PubSub::PubSubFeature supported by the service.
+         * @see Manager::discoverServiceInfos
          */
         virtual void handleServiceInfoResult( const JID& service, int features ) = 0;
 
         /**
-         *
+         * Receives results for a PubSub node info query.
+         * @param service JID of the queried service.
+         * @param node Node ID of the queried node.
+         * @param nodeType PubSub::NodeType of the queried node.
+         * @param df DataForm of the node meta-data (may be empty).
+         * @see Manager::discoverNodeInfos
          */
         virtual void handleNodeInfoResult( const JID& service,
                                            const std::string& node,
@@ -48,17 +61,25 @@ namespace gloox
                                            const DataForm& df ) = 0;
 
         /**
-         *
+         * Receives results for a PubSub item query.
+         * @param service JID of the queried service.
+         * @param parent Node ID of the node to query (empty if the root node
+         *               has been queried).
+         * @see Manager::discoverNodeItems
          */
         virtual void handleNodeItemDiscovery( const JID& service,
                                               const std::string& parent,
                                               const DiscoNodeItemList& children ) = 0;
 
        /**
-        *
+        * Receives error results for a PubSub Disco query.
+        * @param service JID of the queried service.
+        * @param node Node ID of the queried node (empty for the root node).
+        * @param error Error description.
         */
        virtual void handleDiscoError( const JID& service,
-                                      const std::string& parent ) = 0;
+                                      const std::string& node/*,
+                                      const Error& error*/ ) = 0;
 
     };
 
