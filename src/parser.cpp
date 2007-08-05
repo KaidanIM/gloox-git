4f69c7ff60f97d915347bf8d75ac4cedbc013e1d
Jakob Schröter <js@camaya.net>
2007-08-05 19:52:10 +0000

    + support for <![CDATA[ sections
    git-svn-id: svn://svn.camaya.net/gloox/trunk@2776 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/parser.cpp
+++ src/parser.cpp
@@ -90,10 +90,40 @@ namespace gloox
               m_state = TagNameCollect;
               m_preamble = 1;
               break;
+            case '!':
+              if( m_tag.empty() && it + 7 != data.end() && data.substr( i, 8 ) == "![CDATA[" )
+              {
+                it += 7;
+                i += 7;
+                m_state = TagCDATASection;
+              }
+              else
+              {
+                cleanup();
+                return i;
+              }
+              break;
             default:
               m_tag += c;
               m_state = TagNameCollect;
               break;
+          }
+          break;
+        case TagCDATASection:
+          switch( c )
+          {
+            case ']':
+              if( it + 2 != data.end() && data.substr( i, 3 ) == "]]>" )
+              {
+                it += 2;
+                i += 2;
+                addCData();
+                m_state = TagInside;
+                break;
+              } // fall-through intended
+            default:
+            m_cdata += c;
+            break;
           }
           break;
         case TagNameCollect:          // we're collecting the tag's name, we have at least one octet already
@@ -380,9 +410,9 @@ namespace gloox
 
   void Parser::addCData()
   {
-    if( m_current )
+    if( m_current && !m_cdata.empty() )
     {
-      m_current->addCData( relax( m_cdata ) );
+      m_current->addCData( m_state == TagCDATASection ? m_cdata : relax( m_cdata ) );
 //       printf( "added cdata %s to %s: %s\n",
 //               m_cdata.c_str(), m_current->name().c_str(), m_current->xml().c_str() );
       m_cdata = "";
