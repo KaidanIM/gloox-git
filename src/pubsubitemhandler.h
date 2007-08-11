58afc0e02cf4a0686500d29ef3aa394eef6117ce
Vincent Thomasset <vthomasset@gmail.com>
2007-08-11 15:33:55 +0000

    item listing error handling

--- src/pubsubitemhandler.h
+++ src/pubsubitemhandler.h
@@ -46,17 +46,22 @@ namespace gloox
         /**
          * 
          */
-        virtual void handleItem( const JID& service, const std::string& node, const Tag * entry ) = 0;
+        virtual void handleItem( const JID& service,
+                                 const std::string& node,
+                                 const Tag * entry ) = 0;
 
         /**
-         * Receives the list of Items for a node.
+         * Receives the list of Items for a node. Either one of the item list or the
+         * error will be null.
          * @param service Service hosting the queried node.
          * @param nodeid ID of the queried node. If empty, the root node has been queried.
          * @param itemList List of contained items.
+         * @param error Describes the error case if the request failed.
          */
         virtual void handleItemList( const JID& service,
                                      const std::string& node,
-                                     const Tag::TagList * itemList ) = 0;
+                                     const Tag::TagList * itemList,
+                                     const Error * error = 0 ) = 0;
 
 
         virtual void handleItemPublicationResult( const JID& service,
