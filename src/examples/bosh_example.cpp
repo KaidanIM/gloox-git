84b45fc20f0aeacaaab9dd59eb2c7311a2e0f225
Jakob Schröter <js@camaya.net>
2007-09-22 10:59:08 +0000

    - cast warning on certain archs
    git-svn-id: svn://svn.camaya.net/gloox/trunk@3045 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/examples/bosh_example.cpp
+++ src/examples/bosh_example.cpp
@@ -57,9 +57,9 @@ class MessageTest : public MessageSessionHandler, ConnectionListener, LogHandler
        ConnectionTCPClient* conn0 = new ConnectionTCPClient( j->logInstance(), "localhost", 8180 );
        ConnectionBOSH* conn1 = new ConnectionBOSH( j, conn0, j->logInstance(), "localhost", "localhost");
        j->setConnectionImpl( conn1 );
-       
+
        j->setForceNonSasl(); // Needed for non XEP-0206 compliant connection managers (such as Openfire 3.3.x)
-       
+
 
 
       if( j->connect( false ) )
@@ -89,12 +89,14 @@ class MessageTest : public MessageSessionHandler, ConnectionListener, LogHandler
 
     virtual bool onTLSConnect( const CertInfo& info )
     {
+      time_t from( info.date_from );
+      time_t to( info.date_to );
+
       printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\n"
               "from: %s\nto: %s\n",
               info.status, info.issuer.c_str(), info.server.c_str(),
               info.protocol.c_str(), info.mac.c_str(), info.cipher.c_str(),
-              info.compression.c_str(), ctime( (const time_t*)&info.date_from ),
-              ctime( (const time_t*)&info.date_to ) );
+              info.compression.c_str(), ctime( &from ), ctime( &to ) );
       return true;
     }
 
--- src/examples/ft_send.cpp
+++ src/examples/ft_send.cpp
@@ -134,12 +134,14 @@ class FTTest : public LogHandler, ConnectionListener, SIProfileFTHandler, Bytest
 
     virtual bool onTLSConnect( const CertInfo& info )
     {
+      time_t from( info.date_from );
+      time_t to( info.date_to );
+
       printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\n"
               "from: %s\nto: %s\n",
               info.status, info.issuer.c_str(), info.server.c_str(),
               info.protocol.c_str(), info.mac.c_str(), info.cipher.c_str(),
-              info.compression.c_str(), ctime( (const time_t*)&info.date_from ),
-              ctime( (const time_t*)&info.date_to ) );
+              info.compression.c_str(), ctime( &from ), ctime( &to ) );
       return true;
     }
 
