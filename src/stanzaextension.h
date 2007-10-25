7caa2e4f23aebe1ffa6ba323a61b1e09d4dd880a
Jakob Schröter <js@camaya.net>
2007-10-25 20:10:43 +0000

    * no need to have these ORable
    git-svn-id: svn://svn.camaya.net/gloox/trunk@3253 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/stanzaextension.h
+++ src/stanzaextension.h
@@ -28,28 +28,28 @@ namespace gloox
    */
   enum StanzaExtensionType
   {
-    ExtNone                = 1<< 1, /**< Invalid StanzaExtension. */
-    ExtVCardUpdate         = 1<< 2, /**< Extension in the vcard-temp:x:update namespace, advertising
+    ExtNone,                        /**< Invalid StanzaExtension. */
+    ExtVCardUpdate,                 /**< Extension in the vcard-temp:x:update namespace, advertising
                                      * a user avatar's SHA1 hash (XEP-0153). */
-    ExtOOB                 = 1<< 3, /**< An extension in the jabber:iq:oob or jabber:x:oob namespaces
+    ExtOOB,                         /**< An extension in the jabber:iq:oob or jabber:x:oob namespaces
                                      * (XEP-0066). */
-    ExtGPGSigned           = 1<< 4, /**< An extension containing a GPG/PGP signature (XEP-0027). */
-    ExtGPGEncrypted        = 1<< 5, /**< An extension containing a GPG/PGP encrypted message (XEP-0027). */
-    ExtReceipt             = 1<< 6, /**< An extension containing a Message Receipt/Request (XEP-0184). */
-    ExtDelay               = 1<< 7, /**< An extension containing notice of delayed delivery (XEP-0203
+    ExtGPGSigned,                   /**< An extension containing a GPG/PGP signature (XEP-0027). */
+    ExtGPGEncrypted,                /**< An extension containing a GPG/PGP encrypted message (XEP-0027). */
+    ExtReceipt,                     /**< An extension containing a Message Receipt/Request (XEP-0184). */
+    ExtDelay,                       /**< An extension containing notice of delayed delivery (XEP-0203
                                      * & XEP-0091). */
-    ExtAMP                 = 1<< 8, /**< An extension containing advanced message processing rules
+    ExtAMP,                         /**< An extension containing advanced message processing rules
                                      * (XEP-0079). */
-    ExtError               = 1<< 9, /**< An extension containing an error. */
-    ExtCaps                = 1<<10, /**< An extension containing Entity Capabilities (XEP-0115). */
-    ExtChatState           = 1<<11, /**< An extension containing a chat state (XEP-0085). */
-    ExtMessageEvent        = 1<<12, /**< An extension containing a message event (XEP-0022). */
-    ExtDataForm            = 1<<13, /**< An extension containing a Data Form (XEP-0004). */
-    ExtNickname            = 1<<14, /**< An extension containing a User Nickname (XEP-0172). */
-    ExtResourceBind        = 1<<15, /**< A resource bind SE (RFC3921). */
-    ExtVersion             = 1<<16, /**< An extension containing a Version request/reply (XEP-0092). */
-    ExtPSEvent             = 1<<17, /**< An extension for PubSub event notifications (XEP-0060) */
-    ExtUser                = 1<<18  /**< User-supplied extensions must use IDs above this. Do not
+    ExtError,                       /**< An extension containing an error. */
+    ExtCaps,                        /**< An extension containing Entity Capabilities (XEP-0115). */
+    ExtChatState,                   /**< An extension containing a chat state (XEP-0085). */
+    ExtMessageEvent,                /**< An extension containing a message event (XEP-0022). */
+    ExtDataForm,                    /**< An extension containing a Data Form (XEP-0004). */
+    ExtNickname,                    /**< An extension containing a User Nickname (XEP-0172). */
+    ExtResourceBind,                /**< A resource bind SE (RFC3921). */
+    ExtVersion,                     /**< An extension containing a Version request/reply (XEP-0092). */
+    ExtPSEvent,                     /**< An extension for PubSub event notifications (XEP-0060) */
+    ExtUser                         /**< User-supplied extensions must use IDs above this. Do not
                                       * hard-code ExtUser's value anywhere, it is subject to change. */
   };
 
