712bc51992baaa86fd5633cb07c160e4b6bd23cd
Jakob Schröter <js@camaya.net>
2008-01-29 10:59:56 +0000

    + UniqueMUCRoom ported to SE

--- src/stanzaextension.h
+++ src/stanzaextension.h
@@ -69,6 +69,7 @@ namespace gloox
     ExtNonSaslAuth,                 /**< An extension for doing Non-SASL Authentication (XEP-0078). */
     ExtMUCOwner,                    /**< An extension dealing with the muc#owner namespace of XEP-0045. */
     ExtMUCAdmin,                    /**< An extension dealing with the muc#admin namespace of XEP-0045. */
+    ExtMUCUnique,                   /**< An extension dealing with the muc#unique namespace of XEP-0045. */
     ExtPing,                        /**< An XMPP Ping (XEP-0199). */
     ExtUser                         /**< User-supplied extensions must use IDs above this. Do
                                      * not hard-code ExtUser's value anywhere, it is subject
