6622ca01791310dfbfdd6470eeb054be6d94c9df
Jakob Schröter <js@camaya.net>
2008-03-05 02:19:16 +0000

    + non-functional stubs for XEPs 0167, 0176, 0177, 0180

---
+++ src/jinglevideortp.h
@@ -0,0 +1,56 @@
+/*
+  Copyright (c) 2008 by Jakob Schroeter <js@camaya.net>
+  This file is part of the gloox library. http://camaya.net/gloox
+
+  This software is distributed under a license. The full license
+  agreement can be found in the file LICENSE in this distribution.
+  This software may not be copied, modified, sold or distributed
+  other than expressed in the named license agreement.
+
+  This software is distributed without any warranty.
+*/
+
+
+#ifndef JINGLEVIDEORTP_H__
+#define JINGLEVIDEORTP_H__
+
+#include "jingledescription.h"
+
+#include <string>
+
+namespace gloox
+{
+
+  class Tag;
+
+  namespace Jingle
+  {
+
+    /**
+     * @brief An abstraction of the signaling part of Jingle Video via RTP (XEP-0180).
+     *
+     * XEP Version: 0.11
+     * @author Jakob Schroeter <js@camaya.net>
+     * @since 1.0
+     */
+    class VideoRTP : public Description
+    {
+      public:
+        /**
+         * Virtual destructor.
+         */
+        virtual ~VideoRTP() {}
+
+        // reimplemented from Plugin
+        virtual const std::string& filterString() const = 0;
+
+        // reimplemented from Plugin
+        virtual Tag* tag() const = 0;
+
+    };
+
+  }
+
+}
+
+#endif // JINGLEVIDEORTP_H__
