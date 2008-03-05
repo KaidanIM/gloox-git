4b87362e786d38e0e02ab0ee9e0b243a16b3dae4
Jakob Schröter <js@camaya.net>
2008-03-05 02:19:16 +0000

    + non-functional stubs for XEPs 0167, 0176, 0177, 0180
    git-svn-id: svn://svn.camaya.net/gloox/trunk@3648 bb60cc1b-8fe5-0310-9879-aca5d778a633

---
+++ src/jingleaudiortp.h
@@ -0,0 +1,56 @@
+/*
+  Copyright (c) 2008 by Jakob Schroeter <js@camaya.net>
+  This file is part of the gloox library. http://camaya.net/gloox
+
+  This software is distributed under a license. The full license
+  agreement can be found in the file LICENSE in this distribution.
+  This software may not be copied, modified, sold or distributed
+  other than expressed in the named license agreement.
+
+  This software is distributed without any warranty.
+*/
+
+
+#ifndef JINGLEAUDIORTP_H__
+#define JINGLEAUDIORTP_H__
+
+#include "jingledescription.h"
+
+#include <string>
+
+namespace gloox
+{
+
+  class Tag;
+
+  namespace Jingle
+  {
+
+    /**
+     * @brief An abstraction of the signaling part of Jingle Audio via RTP (XEP-0167).
+     *
+     * XEP Version: 0.17
+     * @author Jakob Schroeter <js@camaya.net>
+     * @since 1.0
+     */
+    class AudioRTP : public Description
+    {
+      public:
+        /**
+         * Virtual destructor.
+         */
+        virtual ~AudioRTP() {}
+
+        // reimplemented from Plugin
+        virtual const std::string& filterString() const = 0;
+
+        // reimplemented from Plugin
+        virtual Tag* tag() const = 0;
+
+    };
+
+  }
+
+}
+
+#endif // JINGLEAUDIORTP_H__
