b7505a188ba234de781fadc13bb2a8432451cb7b
Jakob Schröter <js@camaya.net>
2009-02-05 17:34:06 +0000

    * version bump
    git-svn-id: svn://svn.camaya.net/gloox/trunk@3799 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/gloox.cpp
+++ src/gloox.cpp
@@ -108,7 +108,7 @@ namespace gloox
 
   const std::string XMPP_STREAM_VERSION_MAJOR = "1";
   const std::string XMPP_STREAM_VERSION_MINOR = "0";
-  const std::string GLOOX_VERSION           = "1.0-beta4";
+  const std::string GLOOX_VERSION           = "1.0-beta5";
   const std::string GLOOX_CAPS_NODE         = "http://camaya.net/gloox";
 
   const std::string XMLNS = "xmlns";
