48716bfda0e7a83284c4bd19649a28929b99e4cb
Jakob Schröter <js@camaya.net>
2009-10-27 16:59:24 +0000

    * support over-sized TLS fragments + detect TLSv1/SSLv3 from sever-side * better logical-OR cert status
    git-svn-id: svn://svn.camaya.net/gloox/trunk@4278 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/tests/tlsschannel/tlsschannel_test.cpp
+++ src/tests/tlsschannel/tlsschannel_test.cpp
@@ -141,11 +141,12 @@ void SChannelTest::printfCert( CertInfo &info )
 {
       time_t from( info.date_from );
       time_t to( info.date_to );
-      printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\n"
-              "from: %s\nto: %s\n",
+      printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\n",
               info.status, info.issuer.c_str(), info.server.c_str(),
               info.protocol.c_str(), info.mac.c_str(), info.cipher.c_str(),
-              info.compression.c_str(), ctime( &from ), ctime( &to ) );
+              info.compression.c_str() );
+      printf( "from: %s\n", ctime( &from ) );
+      printf( "to: %s\n", ctime( &to) );
 }
 
 std::string SChannelTest::send( const std::string& txt )
