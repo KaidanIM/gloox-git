a1e71e988ce9cccd144acbe68e01cb2829911edd
Jakob Schröter <js@camaya.net>
2009-11-04 16:25:47 +0000

    * fixed timeout again
    git-svn-id: svn://svn.camaya.net/gloox/branches/1.0@4314 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/connectiontcpbase.cpp
+++ src/connectiontcpbase.cpp
@@ -109,7 +109,7 @@ namespace gloox
       return ConnNotConnected;
 
     ConnectionError err = ConnNoError;
-    while( !m_cancel && ( err = recv( 250000 ) ) == ConnNoError )
+    while( !m_cancel && ( err = recv( 1000000 ) ) == ConnNoError )
       ;
     return err == ConnNoError ? ConnNotConnected : err;
   }
