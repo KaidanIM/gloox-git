2b5edccb6fd08bff3b46702c8dccba94947d318b
Jakob Schröter <js@camaya.net>
2010-03-16 17:27:17 +0000

    forwardport of rev 4354 from 1.0 branch

--- src/nonsaslauth.cpp
+++ src/nonsaslauth.cpp
@@ -160,7 +160,6 @@ namespace gloox
             break;
           }
           case TrackSendAuth:
-            m_parent->selectResource( m_parent->jid().resource() );
             m_parent->setAuthed( true );
             m_parent->connected();
             break;
