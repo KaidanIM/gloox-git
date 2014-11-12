7555b734e0d690a7d8b1d2426e99df2e951176c3
Jakob Schröter <js@camaya.net>
2014-11-12 16:30:43 +0000

    * disabled SSLv3
    git-svn-id: svn://svn.camaya.net/gloox/branches/1.0@4557 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/tlsopensslclient.cpp
+++ src/tlsopensslclient.cpp
@@ -30,7 +30,7 @@ namespace gloox
 
   bool OpenSSLClient::setType()
   {
-    m_ctx = SSL_CTX_new( SSLv23_client_method() ); // FIXME: use TLSv1_client_method() as soon as OpenSSL/gtalk combo is fixed!
+    m_ctx = SSL_CTX_new( TLSv1_client_method() );
     if( !m_ctx )
       return false;
 
