8addf10831d2fb2166b9a3acd70bf9e07354acfe
Jakob Schröter <js@camaya.net>
2016-08-02 18:41:56 +0000

    - typo
    git-svn-id: svn://svn.camaya.net/gloox/branches/1.0@4664 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/gloox.h
+++ src/gloox.h
@@ -606,7 +606,7 @@ namespace gloox
   /** Use of Cryptographic Hash Functions in XMPP namespace (@xep{0300}) */
   GLOOX_API extern const std::string XMLNS_HASHES;
 
-  /** IO Data (@xep 0244) */
+  /** IO Data (@xep{0244}) */
   GLOOX_API extern const std::string XMLNS_IODATA;
 
   /** Supported stream version (major). */
