a1960c15eee1acb04d12f5207198b806d21e85c6
Jakob Schröter <js@camaya.net>
2017-02-21 12:20:04 +0000

    * no bits there yet
    git-svn-id: svn://svn.camaya.net/gloox/branches/1.0@4743 bb60cc1b-8fe5-0310-9879-aca5d778a633

--- src/examples/message_example.cpp
+++ src/examples/message_example.cpp
@@ -120,10 +120,10 @@ class MessageTest : public MessageSessionHandler, ConnectionListener, LogHandler
       time_t from( info.date_from );
       time_t to( info.date_to );
 
-      printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\nbits: %d\n",
+      printf( "status: %d\nissuer: %s\npeer: %s\nprotocol: %s\nmac: %s\ncipher: %s\ncompression: %s\n",
               info.status, info.issuer.c_str(), info.server.c_str(),
               info.protocol.c_str(), info.mac.c_str(), info.cipher.c_str(),
-              info.compression.c_str(), info.bits );
+              info.compression.c_str() );
       printf( "from: %s", ctime( &from ) );
       printf( "to:   %s", ctime( &to ) );
       return true;
